f6c23d99d1c492d1de07832935e7a013f090fd9c
vithran21 <56606871+vithran21@users.noreply.github.com>
2024-09-30 22:18:45 +0530

    Added key concepts to card list without custom component in App.js

--- src/App.js
+++ src/App.js
@@ -34,9 +34,19 @@ function App() {
       </header>
       <ul id="concepts">
         <li className="concept">
-          <img src="TODO: IMAGE" alt="TODO: TITLE" />
-          <h2>TODO: TITLE</h2>
-          <p>TODO: DESCRIPTION</p>
+          <img src={concepts[0].image} alt={concepts[0].title} />
+          <h2>{concepts[0].title}</h2>
+          <p>{concepts[0].description}</p>
+        </li>
+        <li className="concept">
+          <img src={concepts[1].image} alt={concepts[1].title} />
+          <h2>{concepts[1].title}</h2>
+          <p>{concepts[1].description}</p>
+        </li>
+        <li className="concept">
+          <img src={concepts[2].image} alt={concepts[2].title} />
+          <h2>{concepts[2].title}</h2>
+          <p>{concepts[2].description}</p>
         </li>
       </ul>
     </div>
