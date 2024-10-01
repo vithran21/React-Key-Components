9eb3ded937e3018c72b16b3747890e0bfb46a164
vithran21 <56606871+vithran21@users.noreply.github.com>
2024-10-01 19:43:22 +0530

    Added Custom Components by segregating Header and Concept

--- src/App.js
+++ src/App.js
@@ -1,53 +1,50 @@
-import keyConceptsImage from './assets/images/key-concepts.png';
-import componentsImage from './assets/images/components.png';
-import stateImage from './assets/images/state.png';
-import eventsImage from './assets/images/events.png';
+import componentsImage from "./assets/images/components.png";
+import stateImage from "./assets/images/state.png";
+import eventsImage from "./assets/images/events.png";
+import Header from "./Components/Header";
+import Concept from "./Components/Concept";
 
 const concepts = [
   {
-    title: 'Components',
+    title: "Components",
     image: componentsImage,
     description:
-      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+      "Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.",
   },
   {
-    title: 'State',
+    title: "State",
     image: stateImage,
     description:
-      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+      "State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.",
   },
   {
-    title: 'Events',
+    title: "Events",
     image: eventsImage,
     description:
-      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+      "Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.",
   },
 ];
 
 function App() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      <Header />
       <ul id="concepts">
-        <li className="concept">
-          <img src={concepts[0].image} alt={concepts[0].title} />
-          <h2>{concepts[0].title}</h2>
-          <p>{concepts[0].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[1].image} alt={concepts[1].title} />
-          <h2>{concepts[1].title}</h2>
-          <p>{concepts[1].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[2].image} alt={concepts[2].title} />
-          <h2>{concepts[2].title}</h2>
-          <p>{concepts[2].description}</p>
-        </li>
+        <Concept
+          title={concepts[0].title}
+          description={concepts[0].description}
+          image={concepts[0].image}
+        />
+        <Concept
+          title={concepts[1].title}
+          description={concepts[1].description}
+          image={concepts[1].image}
+        />
+        <Concept
+          title={concepts[2].title}
+          description={concepts[2].description}
+          image={concepts[2].image}
+        />
       </ul>
     </div>
   );
